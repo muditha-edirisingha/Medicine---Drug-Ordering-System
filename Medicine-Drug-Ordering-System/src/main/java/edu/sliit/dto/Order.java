e6e888e3b42e0acfe16cff832bf394311749d657
muditha-edirisingha <it25100028@my.sliit.lk>
2026-09-14 12:37:07 +0530

    order controller test methode and order dto completed

--- Medicine-Drug-Ordering-System/src/main/java/edu/sliit/dto/Order.java
+++ Medicine-Drug-Ordering-System/src/main/java/edu/sliit/dto/Order.java
@@ -1,4 +1,21 @@
 package edu.sliit.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Order {
+    private Integer orderId;
+    private LocalDateTime orderdate;
+    private String  orderStatus;
+
+    private Double totalAmount;
+    private String deliveryAddress;
 }
