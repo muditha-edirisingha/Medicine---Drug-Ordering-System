8ee3d60947b7dbd0d4d14173f45b659213031588
muditha-edirisingha <it25100028@my.sliit.lk>
2026-09-14 23:47:43 +0530

    update order service

--- Medicine-Drug-Ordering-System/src/main/java/edu/sliit/service/OrderService.java
+++ Medicine-Drug-Ordering-System/src/main/java/edu/sliit/service/OrderService.java
@@ -1,11 +1,16 @@
 package edu.sliit.service;
 
 import edu.sliit.dto.Order;
+import edu.sliit.entity.OrderEntity;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderService {
     List<Order> getOrder();
     void addOrder(Order order);
 
     void deleteByOrderId(Integer orderId);
+
+    Optional<OrderEntity> searchByOrderId(Integer orderId);
 }
