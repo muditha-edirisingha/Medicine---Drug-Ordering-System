325b5887f63a070991664978afd521df790427ae
muditha-edirisingha <it25100028@my.sliit.lk>
2026-09-14 23:30:25 +0530

    Update Order Controller

--- Medicine-Drug-Ordering-System/src/main/java/edu/sliit/Controller/OrderController.java
+++ Medicine-Drug-Ordering-System/src/main/java/edu/sliit/Controller/OrderController.java
@@ -4,6 +4,7 @@ package edu.sliit.Controller;
 import edu.sliit.dto.Order;
 import edu.sliit.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -22,13 +23,22 @@ public class OrderController {
     }
 
     @PostMapping("/add-order")
+    @ResponseStatus(HttpStatus.CREATED)
     public  void addOrder(@RequestBody Order order){
         service.addOrder(order);
 
     }
 
     @DeleteMapping("/delete-by-id/{orderId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteById(@PathVariable Integer orderId){
         service.deleteByOrderId(orderId);
+    }
+
+    @PutMapping("update")
+    @ResponseStatus(HttpStatus.OK)
+    public  void updateOrder(@RequestBody Order order){
+        service.addOrder(order);
+
     }
 }
